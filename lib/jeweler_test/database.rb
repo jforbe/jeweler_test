555f0322515d4a9e5543d6408206c4e677fe2b54
John Forbes <john.forbes@yahoo.com>
2021-09-02 11:41:40 -0700

    made some changes

--- lib/jeweler_test/database.rb
+++ lib/jeweler_test/database.rb
@@ -6,18 +6,27 @@ module JewelerTest
 
   # Configuration defaults
   class Database
+
+    def self.environment
+      env = 'production'
+      env = 'development' if Dir.pwd.include?('Dropbox')
+      env = 'test' if ENV['ENVIRONMENT'] == 'test'
+      env
+    end
+
     def self.connect
       begin
-        config = YAML.load(IO.read('./config/database.yml'))
+        @config = YAML.load(IO.read('./config/database.yml'))
       rescue Errno::ENOENT
-        puts "YAML configuration file couldn't be found. Using defaults."
+        puts "YAML configuration file couldn't be found."
         return
       rescue Psych::SyntaxError
-        puts 'YAML configuration file contains invalid syntax. Using defaults.'
+        puts 'YAML configuration file contains invalid syntax.'
         return
       end
-      config
+      config[environment]
     end
+
   end
 
 end
